d6d6215657cad42ec4c25c58e37b8e5b31847f2c
Jackson Ramos <jackramos021495@gmail.com>
2020-05-05 23:37:07 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(argument)# put argument(s) here
   # code here
+  i = 0
+  while i < argument.length
+    yield argument[i]
+    i = i + 1
+  end
+  argument
 end
