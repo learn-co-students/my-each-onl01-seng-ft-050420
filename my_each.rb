ad5de6df4eae5047f4a33e43fb635775868a048c
Anette Rivera <anettem.rivera@gmail.com>
2020-05-07 16:23:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
-end
+#iterate over each element of an array and do whatever is in the block
+#return the original array 
+
+def my_each(array)
+  idx = 0 
+  while idx < array.length 
+    yield(array[idx]) # <-- (array[idx] iterate every item inside your array)
+    idx += 1 
+  end 
+array
+end 
